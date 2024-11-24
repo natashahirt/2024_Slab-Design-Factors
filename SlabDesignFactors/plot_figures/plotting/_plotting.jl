88b7e8facb9a7d74275d8397aee98698b52971a1
Natasha Hirt <nhirt@mit.edu>
2024-11-24 13:42:54 -0500

    BAU instead of temp_BAU

--- SlabDesignFactors/plot_figures/plotting/_plotting.jl
+++ SlabDesignFactors/plot_figures/plotting/_plotting.jl
@@ -3,9 +3,10 @@ using CSV
 using Colors
 using CairoMakie
 
-include("../../core/constants.jl")
+include("../../core/_core.jl")
 include("assemble_data.jl")
 include("utils.jl")
 
 # plot files
 include("1_multiplot.jl")
+include("2_megaplot.jl")
--- SlabDesignFactors/plot_figures/plotting/run_plots.jl
+++ SlabDesignFactors/plot_figures/plotting/run_plots.jl
@@ -1,9 +1,7 @@
+include("../../scripts/_scripts.jl")
 include("_plotting.jl")
 
 df_all = assemble_data("SlabDesignFactors/results/remote_results/")
 
 plot_1_multiplot(df_all)
 plot_2_megaplot(df_all)
-
-bau_filter = row -> row.name == "r1c2" && row.slab_type == "isotropic" && row.beam_sizer == "discrete" && row.collinear
-println(filter(bau_filter, df_all))
