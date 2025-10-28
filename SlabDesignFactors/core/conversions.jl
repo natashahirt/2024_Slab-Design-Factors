06e0863dd784b1ba30dfcec62d157ee6f1450c40
Natasha Hirt <nhirt@mit.edu>
2025-10-27 20:53:27 -0400

    added more conversions and implemented rebar for multiple zones in beams

--- SlabDesignFactors/core/conversions.jl
+++ SlabDesignFactors/core/conversions.jl
@@ -1,4 +1,5 @@
 kgm3_to_kipin3(kgm3::Real) = kgm3 * 3.6125e-8
+kg_to_kip(kg::Real) = kg * 0.00220462 # convert kg to kilopounds
 
 psf_to_ksi(psf::Real) = psf * 0.000006944444 # convert to ksi
 plf_to_kpi(plf::Real) = (plf / (12 * 1000)) # convert to kip/in
@@ -10,3 +11,7 @@ convert_to_m = Dict(
     :in => 0.0254,      # 1 inch = 0.0254 meters
     :ft => 0.3048       # 1 foot = 0.3048 meters
 )
+
+ft_to_m(ft::Real; factor=1) = ft * 0.3048^factor
+in_to_m(in::Real; factor=1) = in * 0.0254^factor
+kip_per_ft_to_N_per_m(kipperft::Real; factor=1) = kipperft * 14_593.9^factor
